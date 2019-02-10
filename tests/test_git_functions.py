647a5c4e31cf7892f2e590b141827e4fbe0d7be3
Henri-Olivier Duché <hoduche@yahoo.fr>
2019-02-10 18:58:52 +0100

    replace examples by integration tests

--- tests/test_git_functions.py
+++ tests/test_git_functions.py
@@ -8,25 +8,25 @@ good_git_file_2 = '1b44346'
 wrong_git_file = '1b44345'
 
 
-def test_read_git_file():
-    result = ['tree c943c1c80b10a4d18c4852df760ec8ab123d0453',
-              'author Henri-Olivier Duche <Henri-Olivier Duche> 1545516693 +0100',
-              'committer Henri-Olivier Duche <Henri-Olivier Duche> 1545516693 +0100',
-              '',
-              'commit number 1']
-    assert gf.read_git_file(good_path, good_git_file_1) == result
-#    assert gf.read_git_file(good_path, wrong_git_file) is None
-#    assert gf.read_git_file(wrong_path_1, good_git_file_1) == None
-
-
-def test_get_git_objects():
-    blobs, trees, commits, annotated_tags = gf.get_git_objects(good_path)
-    assert blobs == ['6b584e8ece562ebffc15d38808cd6b98fc3d97ea',
-                     'd07ef4424e9caea50099ea1858d3c98c01ece186']
-    assert trees == ['704f9d223bc6288206baeb6b9436634e29fb504c',
-                     'c943c1c80b10a4d18c4852df760ec8ab123d0453']
-    assert commits == ['3d06bbd356e15d9bc2de7e3a6d23311428f90f55',
-                       'd3902329e10a9ed5ff0426eebada3f9c1d01c5cd']
-    assert annotated_tags == ['1b44346eb575773cbbd80644a3e94210afadb6a8',
-                              '4bcf55466d35fa5c17a06300dc256df02b9d3ca1',
-                              'aee3d18ffc66bab391b4a636118dc669bac2fd27']
+#def test_read_git_file():
+#    result = ['tree c943c1c80b10a4d18c4852df760ec8ab123d0453',
+#              'author Henri-Olivier Duche <Henri-Olivier Duche> 1545516693 +0100',
+#              'committer Henri-Olivier Duche <Henri-Olivier Duche> 1545516693 +0100',
+#              '',
+#              'commit number 1']
+#    assert gf.read_git_file(good_path, good_git_file_1) == result
+##    assert gf.read_git_file(good_path, wrong_git_file) is None
+##    assert gf.read_git_file(wrong_path_1, good_git_file_1) == None
+#
+#
+#def test_get_git_objects():
+#    blobs, trees, commits, annotated_tags = gf.get_git_objects(good_path)
+#    assert blobs == ['6b584e8ece562ebffc15d38808cd6b98fc3d97ea',
+#                     'd07ef4424e9caea50099ea1858d3c98c01ece186']
+#    assert trees == ['704f9d223bc6288206baeb6b9436634e29fb504c',
+#                     'c943c1c80b10a4d18c4852df760ec8ab123d0453']
+#    assert commits == ['3d06bbd356e15d9bc2de7e3a6d23311428f90f55',
+#                       'd3902329e10a9ed5ff0426eebada3f9c1d01c5cd']
+#    assert annotated_tags == ['1b44346eb575773cbbd80644a3e94210afadb6a8',
+#                              '4bcf55466d35fa5c17a06300dc256df02b9d3ca1',
+#                              'aee3d18ffc66bab391b4a636118dc669bac2fd27']
--- tests/test_git_graph.py
+++ tests/test_git_graph.py
@@ -1,1 +1,69 @@
-# import git_graph as gg
+import os
+import subprocess
+
+import git_graph.dot_graph as dg
+
+author = "Bernard"
+email = "author@cie.com"
+action_date = "Sat Oct 15 11:00 2011 +0100"
+os.environ["GIT_AUTHOR_NAME"] = author
+os.environ["GIT_COMMITTER_NAME"] = author
+os.environ["GIT_AUTHOR_EMAIL"] = email
+os.environ["GIT_COMMITTER_EMAIL"] = email
+os.environ["GIT_AUTHOR_DATE"] = action_date
+os.environ["GIT_COMMITTER_DATE"] = action_date
+
+
+def execute_command(command, dir0):
+    subprocess.Popen(command, cwd=dir0, stdout=subprocess.PIPE).communicate()
+
+
+def test_full_repo(tmp_path):
+    dir1 = tmp_path / 'dir1'
+    dir1.mkdir()
+    file1 = dir1 / 'file1.txt'
+    file1.write_text('content file1.txt v1')
+    assert len(list(tmp_path.iterdir())) == 1
+    assert file1.is_file()
+    assert file1.read_text() == 'content file1.txt v1'
+
+    file2 = tmp_path / 'file2.txt'
+    file2.write_text('content file2.txt v1')
+    assert len(list(tmp_path.iterdir())) == 2
+    assert file2.is_file()
+    assert file2.read_text() == 'content file2.txt v1'
+
+    execute_command('git init', tmp_path)
+    assert len(list(tmp_path.iterdir())) == 3
+    assert (tmp_path / '.git').is_dir()
+
+    execute_command('git add -A', tmp_path)
+    execute_command('git commit -m "commit1"', tmp_path)
+
+    dg.DotGraph(str(tmp_path)).persist()
+
+    assert len(list(tmp_path.iterdir())) == 4
+    assert (tmp_path / '.gitGraph').is_dir()
+    assert len(list((tmp_path / '.gitGraph').iterdir())) == 2
+    for x in list((tmp_path / '.gitGraph').glob('*.dot')):
+        assert x.read_text() == golden_output
+
+
+golden_output = """digraph auto {
+	graph [bgcolor=transparent]
+	node [fixedsize=true style=filled width=0.95]
+	"54378aec6a6ea34638ac687217745e574360285e" [label="54378ae" fillcolor="#9ccc66"]
+	ecf7586c0df1003ea0a3efa5a00dc3ceaac570f8 [label=ecf7586 fillcolor="#9ccc66"]
+	"6e414aa7e6b2bb6f8bd9fcc652ecd7a349547f92" [label="6e414aa" fillcolor="#bc9b8f"]
+	"6e414aa7e6b2bb6f8bd9fcc652ecd7a349547f92" -> "54378aec6a6ea34638ac687217745e574360285e"
+	"737c972823aec2a30e726cd39821edf8d4b4826b" [label="737c972" fillcolor="#bc9b8f"]
+	"737c972823aec2a30e726cd39821edf8d4b4826b" -> "6e414aa7e6b2bb6f8bd9fcc652ecd7a349547f92"
+	"737c972823aec2a30e726cd39821edf8d4b4826b" -> ecf7586c0df1003ea0a3efa5a00dc3ceaac570f8
+	e53574e083bfb447086df95ad1214d87b6ae45c4 [label=e53574e fillcolor="#85d5fa"]
+	e53574e083bfb447086df95ad1214d87b6ae45c4 -> "737c972823aec2a30e726cd39821edf8d4b4826b"
+	master [label=master fillcolor="#9999ff"]
+	master -> e53574e083bfb447086df95ad1214d87b6ae45c4
+	HEAD [label=HEAD fillcolor="#e6ccff"]
+	HEAD -> master
+}
+"""
