43e2f441683c88d0702206dd0e8db51d568daea0
Henri-Olivier Duché <hoduche@yahoo.fr>
2019-02-17 19:23:02 +0100

    make tests more robust

--- tests/test_git_graph.py
+++ tests/test_git_graph.py
@@ -47,25 +47,29 @@ def test_full_repo(tmp_path):
     assert len(list(tmp_path.iterdir())) == 4
     assert (tmp_path / '.gitGraph').is_dir()
     assert len(list((tmp_path / '.gitGraph').iterdir())) == 2
-    for x in list((tmp_path / '.gitGraph').glob('*.dot')):
-        assert x.read_text() == golden_output
+
+    dot_file_path = list((tmp_path / '.gitGraph').glob('*.dot'))[0]
+    with open(dot_file_path) as dot_file:
+        dot_graph = dot_file.readlines()
+        dot_graph[3:-1] = sorted(dot_graph[3:-1])
+        assert ''.join(dot_graph) == golden_dot_graph
 
 
-golden_output = """digraph auto {
+golden_dot_graph = """digraph auto {
 	graph [bgcolor=transparent]
 	node [fixedsize=true style=filled width=0.95]
 	"54378aec6a6ea34638ac687217745e574360285e" [label="54378ae" fillcolor="#9ccc66"]
-	ecf7586c0df1003ea0a3efa5a00dc3ceaac570f8 [label=ecf7586 fillcolor="#9ccc66"]
-	"6e414aa7e6b2bb6f8bd9fcc652ecd7a349547f92" [label="6e414aa" fillcolor="#bc9b8f"]
 	"6e414aa7e6b2bb6f8bd9fcc652ecd7a349547f92" -> "54378aec6a6ea34638ac687217745e574360285e"
-	"737c972823aec2a30e726cd39821edf8d4b4826b" [label="737c972" fillcolor="#bc9b8f"]
+	"6e414aa7e6b2bb6f8bd9fcc652ecd7a349547f92" [label="6e414aa" fillcolor="#bc9b8f"]
 	"737c972823aec2a30e726cd39821edf8d4b4826b" -> "6e414aa7e6b2bb6f8bd9fcc652ecd7a349547f92"
 	"737c972823aec2a30e726cd39821edf8d4b4826b" -> ecf7586c0df1003ea0a3efa5a00dc3ceaac570f8
-	e53574e083bfb447086df95ad1214d87b6ae45c4 [label=e53574e fillcolor="#85d5fa"]
-	e53574e083bfb447086df95ad1214d87b6ae45c4 -> "737c972823aec2a30e726cd39821edf8d4b4826b"
-	master [label=master fillcolor="#9999ff"]
-	master -> e53574e083bfb447086df95ad1214d87b6ae45c4
-	HEAD [label=HEAD fillcolor="#e6ccff"]
+	"737c972823aec2a30e726cd39821edf8d4b4826b" [label="737c972" fillcolor="#bc9b8f"]
 	HEAD -> master
+	HEAD [label=HEAD fillcolor="#e6ccff"]
+	e53574e083bfb447086df95ad1214d87b6ae45c4 -> "737c972823aec2a30e726cd39821edf8d4b4826b"
+	e53574e083bfb447086df95ad1214d87b6ae45c4 [label=e53574e fillcolor="#85d5fa"]
+	ecf7586c0df1003ea0a3efa5a00dc3ceaac570f8 [label=ecf7586 fillcolor="#9ccc66"]
+	master -> e53574e083bfb447086df95ad1214d87b6ae45c4
+	master [label=master fillcolor="#9999ff"]
 }
 """
